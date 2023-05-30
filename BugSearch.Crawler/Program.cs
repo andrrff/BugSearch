3c2aeb4087f5f60ce5e24d803f15fc21ecbacce0
André da Silva Freitas <andre.freitas@i9tec.com.br>
2023-05-30 01:48:50 -0300

    Set config for CORS

--- BugSearch.Crawler/Program.cs
+++ BugSearch.Crawler/Program.cs
@@ -7,16 +7,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", builder =>
-    {
-        builder.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
-    });
-});
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
