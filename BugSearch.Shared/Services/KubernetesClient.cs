dd10b235b1b277c48456fa3491198d011ef5f8b4
André da Silva Freitas <andre.freitas@i9tec.com.br>
2023-05-31 21:43:33 -0300

    Adjust workflow adding no-ignore

--- BugSearch.Shared/Services/KubernetesClient.cs
+++ BugSearch.Shared/Services/KubernetesClient.cs
@@ -6,23 +6,23 @@ namespace BugSearch.Shared.Services;
 
 public class KubernetesClient
 {
-    // public static string GetSecret(string name, string data, string ns = "crawler-bot")
-    // {
-    //     var config = KubernetesClientConfiguration.InClusterConfig();
-    //     var client = new Kubernetes(config);
-    //     var secret = client.ReadNamespacedSecret(name, ns);
+    public static string GetSecret(string name, string data, string ns = "crawler-bot")
+    {
+        var config = KubernetesClientConfiguration.InClusterConfig();
+        var client = new Kubernetes(config);
+        var secret = client.ReadNamespacedSecret(name, ns);
 
-    //     return Encoding.UTF8.GetString(secret.Data[data]);
-    // }
+        return Encoding.UTF8.GetString(secret.Data[data]);
+    }
 
-    // public static string GetConfigMap(string name, string data, string ns = "crawler-bot")
-    // {
-    //     var config    = KubernetesClientConfiguration.InClusterConfig();
-    //     var client    = new Kubernetes(config);
-    //     var configMap = client.ReadNamespacedConfigMap(name, ns);
+    public static string GetConfigMap(string name, string data, string ns = "crawler-bot")
+    {
+        var config    = KubernetesClientConfiguration.InClusterConfig();
+        var client    = new Kubernetes(config);
+        var configMap = client.ReadNamespacedConfigMap(name, ns);
 
-    //     return configMap.Data[data];
-    // }
+        return configMap.Data[data];
+    }
 
     public static string GetAllSecrets(string ns = "crawler-bot")
     {
