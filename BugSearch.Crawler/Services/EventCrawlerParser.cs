b0fc692414988c5336f28b6cdb6022c5b5a8e9a5
André da Silva Freitas <andrecastanhal@gmail.com>
2023-05-31 23:37:01 -0300

    Adjust statement in crawlerbot

--- BugSearch.Crawler/Services/EventCrawlerParser.cs
+++ BugSearch.Crawler/Services/EventCrawlerParser.cs
@@ -15,7 +15,7 @@ class EventCrawlerParser : DataParser
             var title       =  context.Selectable.XPath(".//title")?.Value;
             var favicon     =  context.Selectable.XPath(".//link[@rel='icon']")?.Value;
             var description =  context.Selectable.XPath(".//meta[@name='description']")?.Value ?? string.Empty;
-            var body        =  Regex.Replace(Regex.Replace(Regex.Unescape(Regex.Replace(Regex.Replace(context.Selectable.XPath(".//body").Value, @"\s+", " ").Trim(), @"\W+", "")), @"(\B[A-Z])", " $1"), @"([a-z])([A-Z])", "$1 $2");
+            var body        =  Regex.Replace(Regex.Replace(Regex.Replace(Regex.Unescape(Regex.Replace(Regex.Replace(context.Selectable.XPath(".//body").Value, @"\s+", " ").Trim(), @"\W+", "")), @"(\B[A-Z])", " $1"), @"([a-z])([A-Z])", "$1 $2"), $@"\b\w{{1,{2 - 1}}}\b", "");
             var terms       =  body.Split(" ").Distinct().Select(term => term.ToLower()).ToArray();
 
             if (!string.IsNullOrEmpty(title) || string.IsNullOrEmpty(body))
