46c329b1b5a94b619340c1e72fe2256d804adf4d
André da Silva Freitas <andrecastanhal@gmail.com>
2023-06-01 18:11:15 -0300

    Remove test from api

--- BugSearch.Api/Controllers/TestController.cs
+++ BugSearch.Api/Controllers/TestController.cs
@@ -4,30 +4,30 @@ using BugSearch.Shared.Services;
 
 namespace BugSearch.Api.Controllers;
 
-[ApiController]
-[Route("[controller]")]
-public class TestController : ControllerBase
-{
-    private readonly ILogger<TestController> _logger;
+// [ApiController]
+// [Route("[controller]")]
+// public class TestController : ControllerBase
+// {
+//     private readonly ILogger<TestController> _logger;
 
-    // private readonly DatabaseConntection _context;
+//     // private readonly DatabaseConntection _context;
 
-    public TestController(ILogger<TestController> logger)
-    {
-        _logger = logger;
-        // _context = new DatabaseConntection();
-    }
+//     public TestController(ILogger<TestController> logger)
+//     {
+//         _logger = logger;
+//         // _context = new DatabaseConntection();
+//     }
 
-    [HttpGet(Name = "Test env")]
-    public string Get()
-    {
-        try
-        {
-            return Environment.GetEnvironmentVariable("OPENAI_KEY") ?? string.Empty;
-        }
-        catch (Exception ex)
-        {
-            return ex.ToString();
-        }
-    }
-}
+//     [HttpGet(Name = "Test env")]
+//     public string Get()
+//     {
+//         try
+//         {
+//             return Environment.GetEnvironmentVariable("OPENAI_KEY") ?? string.Empty;
+//         }
+//         catch (Exception ex)
+//         {
+//             return ex.ToString();
+//         }
+//     }
+// }
